05c87217a4fde141b136011f438f6e20b491f6e0
niven90 <yuriy.sh.90@gmail.com>
2018-05-22 21:02:50 +0300

    init new project

---
+++ script.js
@@ -0,0 +1,71 @@
+/*дз 1.1*/
+/*
+var city = "Kyiv";
+alert(city);
+city = "Lviv";
+alert(city);
+var address = city;
+alert(address);
+
+*/
+/*дз 1.2*/
+/*var n = 3;
+var result = n*n*n;
+alert(result);*/
+/*дз 1.3*/
+/*
+var n = 5.25;
+var p = 3.50;
+var na = n * 4;
+var pa = p * 6;
+var rezult = pa + na;
+alert(rezult);*/
+/*дз 1.4 не зроблено*/
+
+let x;
+let n = x * 2 / 6 + 8
+if(n == 24){
+alert(x);
+}
+
+
+
+
+  
+
+
+//(2 * х / 6 + 8 = 24)
+//
+//let a = 24;
+//alert(24 = 2 * x / 6 + 8);
+
+/*дз 2.1*/
+//let n1 = +prompt('skilku godun y dobi?');
+//if (n1 = '24') {
+//    i++
+//}
+//else(i);
+//let n2 = +prompt('skilku godun y dobi?');
+//if (n2 = '24') {
+//    i++
+//}
+//else(i+);
+//let n3 = +prompt('skilku godun y dobi?');
+//if (n3 = '24') {
+//    i++
+//}
+//else(i+);
+//alert(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
